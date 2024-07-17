d238255bbb00e990b5e28eb17e01b606d75c6c82
eg-delacruz <netoxas3107@hotmail.com>
2024-07-17 11:53:22 +0200

    fix: redo accidentally erased files and changes when untracking .env file

--- app/(protected_routes)/dashboard/page.tsx
+++ app/(protected_routes)/dashboard/page.tsx
@@ -1,22 +1,23 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+'use client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 //Auth
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 
 //Firestore DB
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import { app } from "@config/FirebaseConfig";
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { app } from '@config/FirebaseConfig';
 
 //Utils
-import createSchedulerUser from "@utils/createSchedulerUser";
+import createSchedulerUser from '@utils/createSchedulerUser';
 
 //Context
-import { useAppContext } from "@context/index";
+import { useAppContext } from '@context/index';
 
 //Components
-import Meetings from "@/app/(protected_routes)/dashboard/_components/Meetings";
+import Meetings from '@/app/(protected_routes)/dashboard/_components/Meetings';
+import Loader from '@components/Loader';
 
 //TODO: display a proper loading screen
 //TODO: instead of redirecting to meeting-type, display the meeting list here
@@ -47,30 +48,30 @@ function Dashboard() {
 
   //Auth redirect
   if (!isLoading && !isAuthenticated) {
-    router.replace("/api/auth/login");
+    router.replace('/api/auth/login');
   }
 
   //We first check if there is a registered business before showing the dashboard. If not, we redirect user to create a business
   const isBusinessRegistered = async () => {
     //We access the document called like the user email stored in the Business collection
-    const docRef = doc(db, "Business", String(user?.email));
+    const docRef = doc(db, 'Business', String(user?.email));
     const docSnap = await getDoc(docRef);
 
     //Based on the user email, we look for a document named like it. That document means that we have created a business before and we can start scheduling
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      console.log('Document data:', docSnap.data());
       setLoading(false);
-      router.replace("/dashboard/meeting-type");
+      router.replace('/dashboard/meeting-type');
     } else {
-      console.log("No such document!");
+      console.log('No such document!');
       setLoading(false);
-      router.replace("/create-business");
+      router.replace('/create-business');
     }
   };
 
   const isSchedulerUserCreated = async () => {
     //Ref to db, collection and document name
-    const docRef = doc(db, "SchedulerUser", String(user?.email));
+    const docRef = doc(db, 'SchedulerUser', String(user?.email));
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
@@ -81,17 +82,21 @@ function Dashboard() {
       if (user) {
         const SchedulerUser = await createSchedulerUser(user);
         setContext(SchedulerUser);
-        console.log("User created");
+        console.log('User created');
         setLoading(false);
       }
     }
   };
 
-  if (loading || isLoading || !SchedulerUser || true) {
-    return <div>Loading</div>;
+  if (loading || isLoading || !SchedulerUser) {
+    return (
+      <div className='flex items-center justify-center container h-[90vh]'>
+        <Loader />
+      </div>
+    );
   }
 
-  return <Meetings />;
+  return <Meetings SchedulerUser={SchedulerUser} />;
 }
 
 export default Dashboard;
